d48765b0cca84e3b36d3770f196de83f82d63a88
cckk00 <cckk00@126.com>
2020-03-13 09:00:59 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+let path = require('path');
+
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: '[name][hash].css'
+});
+
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './index.html',
+  inject: true,
+  minify: {
+    removeComments: true,
+    removeAttributeQuotes: true,
+    collapseWhitespace: true
+  },
+  filename: 'index.html'
+});
+
+module.exports = {
+
+  mode: 'production',
+
+  entry: {
+    index: ['./js/index.js']
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name][hash].min.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: 'css-loader'}
+        ]
+      },
+
+      {
+        test: /\.(png|gif|jpg|jpeg|webp)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5120,
+              esModule: false
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.html?$/,
+        use: [
+          {loader: 'html-withimg-loader'}
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    miniCssExtractPlugin,
+    htmlWebpackPlugin
+  ],
+
+  devServer: {
+    host: 'localhost',
+
+    port: 8002
+  }
+
+}
